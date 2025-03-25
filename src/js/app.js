4bef1153f60dfcb09c7e8d7e74a5713ab387f24f
ImJAraya <fabiarayamontero@gmail.com>
2025-03-25 22:22:33 +0000

    Terminado

--- src/js/app.js
+++ src/js/app.js
@@ -33,7 +33,7 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} == null ? Name : ${variables.name} ${variables.lastName} == null ? lastName : ${variables.lastName}</h1>
+          <h1>${variables.name} ${variables.lastName}</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
