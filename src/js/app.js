cff6e264f74b65f1530b9c77ebd78eb2a621d6c9
ImJAraya <fabiarayamontero@gmail.com>
2025-03-26 21:11:02 +0000

    cambiando el null

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,25 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastName}</h1>
+          <h1><h1>${variables.name !== "null" ? variables.name : ""} ${
+    variables.lastName !== "null" ? variables.lastName : ""
+  }</h1>
+</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://${variables.github}"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://www.linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://x.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://www.instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
